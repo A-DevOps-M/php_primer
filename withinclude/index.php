f69ebd6c529fee1ea3933655b942cf1f29c4a130
A-DevOps-M <arammousa@outlook.com>
2021-04-17 07:59:14 +0200

    added buttons

--- withinclude/index.php
+++ withinclude/index.php
@@ -8,4 +8,10 @@
     <h2>Hello From The Home Page</h2>
 
 
+
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+
     <?php require 'includes/footer.php'; ?>
